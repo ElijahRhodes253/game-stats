e05ecf0552520b799a89b9dc74d90283832d3362
Elijah_Rhodes <mastergamer3dr@gmail.com>
2026-05-03 15:26:56 -0700

    Completed getAverageScore() method

--- src/MapGameStatsCalculator.java
+++ src/MapGameStatsCalculator.java
@@ -134,11 +134,25 @@ public class MapGameStatsCalculator implements GameStatsCalculator {
    */
   @Override
   public double getAverageScore(String person) {
-    // TODO: remove this exception once you have implemented your method!
-    throw new UnsupportedOperationException("Unimplemented method 'getAverageScore'");
+    
+    checkPerson(person);
+    List<Integer> scoresList = null;
 
-    // Uncomment this and have it as your first line once you remove the UnsupportedOperationException
-    //checkPerson(person);
+    for(String x : allScores.keySet())
+    {
+      if(x.equals(person)){scoresList = allScores.get(x);}
+    }
+
+    double avg;
+    int tot = 0;
+    int scoreSize = scoresList.size();
+    for(int i = 0; i < scoreSize; i++)
+    {
+      tot += scoresList.get(i);
+    }
+
+    avg = (tot * 1.0) / (scoreSize * 1.0);
+    return avg;
   }
 
   /**
